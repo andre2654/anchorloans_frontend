9dd6e9304d4974a942304952057973f58e9922d2
Saraiva <andre.victor29@hotmail.com>
2022-02-11 22:13:18 -0300

    custom url api added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ module.exports = {
   env: {
     AWS_ACCESSKEY_ID: 'AKIAW6BKNUPWXHHDO2UP',
     AWS_ACCESSKEY_KEY: 'rHc2G8f/OMcD0zEXI/fwNXOj2RyNOTnKUGA820dL',
+    FRONTEND_URL_VERCEL: 'https://anchorloans-frontend.vercel.app/',
+    FRONTEND_URL_DEV: 'http://localhost:3000/',
     BACKEND_URL_HEROKU: 'https://anchorloansbackend.herokuapp.com/',
     BACKEND_URL_DEV: 'http://127.0.0.1:5000/'
   },
--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'http://localhost:3000/'
+	baseURL: process.env.NODE_ENV == 'development' ? process.env.FRONTEND_URL_DEV : process.env.FRONTEND_URL_VERCEL
 });
 
 api.defaults.withCredentials = true;
